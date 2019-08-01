e0a9c0a0572bb04532cb5e464afc53dfdc8de6b6
sundeep <sunguru98@yahoo.co.in>
2019-08-02 00:39:49 +0530

    Completed some of the landing page section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+// Components
 import NavBar from './components/NavBar/NavBar'
 import Footer from './components/Footer'
+// Pages
+import LandingPage from './pages/LandingPage'
+// React-router
+import { Switch, Route, Redirect } from 'react-router-dom'
+
 function App() {
   return (
     <div className="App">
       <NavBar />
       <div className='container' style={{ padding: '1.5rem 2.2rem' }}>
-
+        <Switch>
+          <Route exact path='/' component={LandingPage} />
+          <Redirect to='/' />
+        </Switch>
       </div>
       <Footer />
     </div>
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import larrow from '../images/larrow.svg'
+import rarrow from '../images/rarrow.svg'
+import carouselImg from '../images/carouselimg.png'
+
+import '../styles/pages/LandingPage.scss'
+
 const LandingPage = (props) => {
   return (
     <section className='LandingPage'>
-      
+      <div className='LandingPage__welcome'>
+        <div className='LandingPage__welcome--carousel' style={{ backgroundImage: `url(${carouselImg})` }}>
+          <img src={larrow} className='LandingPage__welcome--carousel-left' alt='left' />
+          <img src={rarrow} className='LandingPage__welcome--carousel-left' alt='right' />
+        </div>
+        <div className='LandingPage__helpform'>
+          <h2 className='LandingPage__helpform-title'>
+            Want us to call you back ?
+          </h2>
+          <form className='LandingPage__helpform-form'>
+            <label htmlFor='name'>Name</label>
+            <input type='text' id='name' name='name' />
+            <label htmlFor='phnumber'>Phone Number</label>
+            <input id='phnumber' type='tel' name='phoneNumber'/>
+            <label htmlFor='message'>Message</label>
+            <input id='message' type='text'/>
+            <input type='submit' value='Request Callback' />
+          </form>
+        </div>
+      </div>
     </section>
   )
 }
